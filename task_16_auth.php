f0ac4d92208f5191398f6e7dab32b11fb46cc2d4
kozyrev42 <kozyrev054@gmail.com>
2022-06-01 16:09:33 +0700

    задача 16 в работе

--- task_16_auth.php
+++ task_16_auth.php
@@ -1,2 +1,50 @@
 <?php
+session_start();
 // обработчик формы авторизации
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+require_once('connect_bd.php');
+// проверка есть ли в базе емаил
+// запрос, с меткой на которую передадим переменную
+$query = "SELECT * FROM `users-13` WHERE email=:email";
+// нужно подготовить запрос, для безопасной отправки в бд
+$statement = $pdo->prepare($query);
+// в запросе, на метку передаём переменную и выполняем его
+$statement -> execute(['email' => $email]);
+// $result - может содержать запись из таблицы
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+//var_dump($result);
+// если данные пришли, значит такой емаил в бд есть, нужно перейти проверки пароля
+
+if (empty($result)) { // если $result пустой выполняем блок
+    // Ошибка в переменной Сессии true, то есть активна
+    $_SESSION['error'] = true;
+    // возращаемся к форме
+    header ('location: task_16_auth_form.php');
+    // прекращаем дальнейщее исполнение скрипта
+    exit;
+}
+
+
+
+
+// запрос в бд, сравниваем пароли
+/* $query = "SELECT * FROM `users-13` WHERE email=:email";
+$statement = $pdo->prepare($query);
+$statement -> execute(['email' => $email]);
+$result = $statement->fetch(PDO::FETCH_ASSOC); */
+
+
+// пароли не равны, записываем в сессию Ошибку
+// возращаем на форму входа
+// прерываем сценарий
+// exit;
+
+
+// если всё хорошо
+// записывем в сессию емаил
+// возвращаем на главную
+// header ('location: task_16_index.php');
--- task_16_index.php
+++ task_16_index.php
@@ -1,3 +1,61 @@
 <?php
+session_start();
 // в бд используем таблицу users-13
 // стартовая страница 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>
+        Подготовительные задания к курсу
+    </title>
+    <meta name="description" content="Chartist.html">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+    <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+    <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+    <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+    <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <!-- если сессия НЕ содержит пользователя -->
+            <div class="col-md-4">
+                <h2> Вы не авторизованы </h2>
+                <div>
+                    <a href="task_16_auth_form.php" class="btn btn-info">Войти</a>
+                </div>
+            </div>
+            <!-- если сессия содержит пользователя -->
+            <!-- выводим емаил пользователя -->
+            <div class="col-md-4">
+                <h2> Вы вошли как </h2>
+                <div>
+                    <a href="task_16_logout.php" class="btn btn-danger">Выйти</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+    <script src="js/vendors.bundle.js"></script>
+    <script src="js/app.bundle.js"></script>
+    <script>
+    // default list filter
+    initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+    // custom response message
+    initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+    </script>
+</body>
+
+</html>
